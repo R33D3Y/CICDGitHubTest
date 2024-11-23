1ed649984f4937e488c7b8199a494e512c04eea6
R33D3Y <reedey2708@gmail.com>
2024-11-23 12:31:21 +0000

    Add project files.

---
+++ CICDGitHubTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CICDGitHubTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
